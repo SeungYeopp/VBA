2bab95d8948549f48639e3840ddd6052bf3608d9
SeungHeon <shun8800@softzen.co.kr>
2023-07-25 16:54:18 +0900

    week9

---
+++ Programmers/Week9/87946.py
@@ -0,0 +1,12 @@
+# 피로도: https://school.programmers.co.kr/learn/courses/30/lessons/87946
+
+def solution(k, dungeons):
+    answer = -1
+
+    for t, t_consume in dungeons:
+        pass
+
+    return answer
+
+
+print(solution(80, [[80,20],[50,40],[30,10]]))
